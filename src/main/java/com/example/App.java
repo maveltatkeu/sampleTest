fd0cedbf58d548ae2007d2f5846865fb92b9cd44
mavel.tatkeu.mbiakop <mavel.tatkeu.mbiakop@accenture.com>
2024-07-25 12:31:59 +0400

    added display method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,26 @@
 package com.example;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App {
+    static Logger logger = Logger.getLogger(App.class.getName());
     public static void main(String... args) {
-        System.out.println("Hello");
+
+        logger.info("Hello");
+        display();
+    }
+    public static void display( ) {
+        logger.info("  _____  _____");
+        logger.info(" /     \\/     |");
+        logger.info(" |  |\\    /|  | ");
+        logger.info(" |  | \\__/ |  | ");
+        logger.info(" |  |      |  | ");
+        logger.info(" |__|      |__| ");
+
     }
 
 }
